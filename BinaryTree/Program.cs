b5528c77a26b9eb625448ccdd31079d45d1d745f
Lena <lenaLatkovich@gmail.com>
2018-04-02 03:46:54 +0300

    add balancing

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -13,12 +13,40 @@ namespace BinaryTree {
 
 			myTree.Remove(5);
 
-			//myTree.AddNode(new Node<int>(4));
-			//myTree.AddNodes(new []{ new Node<int>(2), new Node<int>(1), new Node<int>(3)});
-			//myTree.AddValue(5);
-			//myTree.AddValues(new[ ] { 7, 6, 8 });
+			Tree<int> myTreeSecond = new Tree<int>("Second");
+			myTreeSecond.AddNode(new Node<int>(4));
+			myTreeSecond.AddNodes(new[ ] { new Node<int>(2), new Node<int>(1), new Node<int>(3) });
+			myTreeSecond.AddValue(5);
+			myTreeSecond.AddValues(new[ ] { 7, 6, 8 });
 
-			//myTree.Remove(5);
+			myTree.Balancing( );
+
+			myTree.Remove(5);
+
+			if (myTree.IsBalance(myTree.Root)) {
+				Console.WriteLine("Tree is balances");
+			} else {
+				Console.WriteLine("Tree isn't balances");
+			}
+			myTree.AutoBalances( );
+
+			Tree<int> myTreeTherd = new Tree<int>("Therd");
+			myTreeTherd.AddValues(new[ ] { 50, 25, 15, 10, 9, 12, 18, 17, 43, 30, 28, 45, 44, 47, 75, 60 });
+			myTreeTherd.Remove(15);
+			myTreeTherd.Remove(18);
+			if (myTreeTherd.IsBalance(myTreeTherd.Root)) {
+				Console.WriteLine("Tree is balances");
+			} else {
+				Console.WriteLine("Tree isn't balances");
+			}
+			Console.ReadLine( );
+			myTreeTherd.Balancing( );
+			if (myTree.IsBalance(myTreeTherd.Root)) {
+				Console.WriteLine("Tree is balances");
+			} else {
+				Console.WriteLine("Tree isn't balances");
+			}
+
 
 			myTree.Inorder( );
 			myTree.Preorder( );
--- BinaryTree/Tree.cs
+++ BinaryTree/Tree.cs
@@ -8,6 +8,7 @@ namespace BinaryTree {
 	class Tree<Tn> where Tn : IComparable {
 		public string Title;
 		public Node<Tn> Root = null;
+		public bool autoBalancing = false;
 
 		private enum SIDE {
 			LEFT, RIGHT, NONE
@@ -15,6 +16,15 @@ namespace BinaryTree {
 
 		public Tree(string _title) {
 			this.Title = _title;
+		}
+
+		public bool AutoBalances( ) {
+			if (autoBalancing) {
+				Console.WriteLine("Autobalancing off.");
+			} else {
+				Console.WriteLine("Autobalancing on.");
+			}
+			return !autoBalancing;
 		}
 
 		public void AddNode(Node<Tn> newNode) {
@@ -25,8 +35,8 @@ namespace BinaryTree {
 			}
 		}
 
-		public void AddNodes(Node<Tn>[] newNodes) {
-			foreach(Node<Tn> node in newNodes) {
+		public void AddNodes(Node<Tn>[ ] newNodes) {
+			foreach (Node<Tn> node in newNodes) {
 				this.AddNode(node);
 			}
 		}
@@ -35,8 +45,8 @@ namespace BinaryTree {
 			this.AddNode(new Node<Tn>(value));
 		}
 
-		public void AddValues(Tn[] values) {
-			foreach(Tn value in values) {
+		public void AddValues(Tn[ ] values) {
+			foreach (Tn value in values) {
 				this.AddValue(value);
 			}
 		}
@@ -121,6 +131,9 @@ namespace BinaryTree {
 					node = node.Right;
 				}
 			}
+			if (autoBalancing) {
+				this.Balancing( );
+			}
 		}
 
 
@@ -137,6 +150,9 @@ namespace BinaryTree {
 				} else {
 					InsertPlace(startNode.Right, newNode);
 				}
+			}
+			if(autoBalancing) {
+				this.Balancing( );
 			}
 		}
 
@@ -169,7 +185,7 @@ namespace BinaryTree {
 				LRC(current.Left);
 				LRC(current.Right);
 			}
-		} 
+		}
 
 		public void Postorder( ) {
 			Console.Write("Postorder:");
@@ -183,6 +199,77 @@ namespace BinaryTree {
 				RCL(current.Right);
 				RCL(current.Left);
 				Console.Write(" " + current.Key);
+			}
+		}
+
+		public bool IsBalance(Node<Tn> node) {
+			if (node == null) {
+				return true;
+			} else {
+				bool leftIsBalance = IsBalance(node.Left);
+				bool rightIsBalance = IsBalance(node.Right);
+				int leftCountLevel = CountLevel(node.Left);
+				int rightCountLevel = CountLevel(node.Right);
+				if (leftIsBalance && rightIsBalance && Math.Abs(leftCountLevel - rightCountLevel) <= 1) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+
+		private int CountLevel(Node<Tn> node) {
+			if (node == null) {
+				return 0;
+			} else {
+				int leftCountLevel = 1 + CountLevel(node.Left);
+				int rightCountLevel = 1 + CountLevel(node.Right);
+				if (leftCountLevel > rightCountLevel) {
+					return leftCountLevel;
+				} else {
+					return rightCountLevel;
+				}
+			}
+		}
+
+		public void Balancing( ) {
+			List<Tn> listNodes = new List<Tn>( );
+			getListNodes(this.Root, listNodes);
+			this.Root = null;
+			CreateBalanceTree(this, listNodes);
+		}
+
+		private List<Tn> getListNodes(Node<Tn> current, List<Tn> listNodes) {
+			if (current != null) {
+				getListNodes(current.Left, listNodes);
+				listNodes.Add(current.Key);
+				getListNodes(current.Right, listNodes);
+			}
+			return listNodes;
+		}
+
+		private void CreateBalanceTree(Tree<Tn> balancTree, List<Tn> listNodes) {
+			List<Tn> listLeftNodes = new List<Tn> { };
+			List<Tn> listRightNodes = new List<Tn> { };
+			int centerList = 0;
+			if (listNodes.Count % 2 == 0) {
+				centerList = Convert.ToInt16(Math.Ceiling(listNodes.Count / 2.0));
+			} else {
+				centerList = Convert.ToInt16(Math.Floor(listNodes.Count / 2.0));
+			}
+			if (listNodes.Count > 1) {
+					balancTree.AddNode(new Node<Tn>(listNodes[centerList]));
+				for (int index = 0; index < listNodes.Count; index++) {
+					if (index < centerList) {
+						listLeftNodes.Add(listNodes[index]);
+					} else if (index > centerList) {
+						listRightNodes.Add(listNodes[index]);
+					}
+				}
+				CreateBalanceTree(balancTree, listLeftNodes);
+				CreateBalanceTree(balancTree, listRightNodes);
+			} else if (listNodes.Count == 1) {
+				balancTree.AddNode(new Node<Tn>(listNodes[0]));
 			}
 		}
 	}
